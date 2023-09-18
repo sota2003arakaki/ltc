b157d1d7ffa0bdc64fe1ba36b7ed141238abeb2d
sota2003arakaki <sota2003arakaki@gmail.com>
2023-09-19 01:14:10 +0900

    detail centext

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '_', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <h1 class="title">
+            {{ $post->title }}
+        </h1>
+        <div class="content">
+            <div class="content__post">
+                <h3>本文</h3>
+                <p>{{ $post->body }}</p>
+            </div>
+        </div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
