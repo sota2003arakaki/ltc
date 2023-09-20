71b58ff4fac8706d685989f8c02c472051831234
sota2003arakaki <sota2003arakaki@gmail.com>
2023-09-20 11:40:38 +0900

    baridertion

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,30 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
-/**
- * Post一覧を表示する
- * 
- * @param Post Postモデル
- * @return array Postモデルリスト
- */
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]) ;
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
     }
+
     public function create()
     {
         return view('posts.create');
     }
-    public function store(Request $request, Post $post)
+
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
     {
         $input = $request['post'];
         $post->fill($input)->save();
